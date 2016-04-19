4c2b86b154d2aa4d3e59570e715ed3664d890b21
bublak <pavel.filipcik@gmail.com>
2016-04-19 23:02:48 +0200

    not finished: add support for recounting ID columns -> also recount the references in other tables - NOTE: contains bug

--- fixturesGenerator.php
+++ fixturesGenerator.php
@@ -9,10 +9,13 @@ ini_set('xdebug.var_display_max_data', -1);
 
 $fixtures = '';
 
+$recountData = array();
+
 $fixtures = createFixtures($tableData, $defaultColumnsData, $startDependData);
 
 echo '<br><br><br>';
 var_dump($fixtures);
+
 
 function createFixtures($tableData, $defaultColumnsData, $dependData=null) {
     $fixtures = '';
@@ -40,14 +43,14 @@ function createFixtures($tableData, $defaultColumnsData, $dependData=null) {
 }
 
 function doSelect ($table, $filterData, &$fixtures, $collect, $dependData=array()) {
-    global $defaultColumnsData;
+    global $defaultColumnsData, $recountData;
 
     $limit = $filterData['limit'];
     $order = $filterData['order'];
 
     $select = "SELECT * from $table";
 
-    $selCondition = '';
+    $selCondition = $collectionColumn = '';
 
     $conjunction = false;
 
@@ -59,7 +62,10 @@ function doSelect ($table, $filterData, &$fixtures, $collect, $dependData=array(
         if ($fData['val']) {
             $filterCondition = $fData['val'];
         } else {
-            $filterCondition = $dependData[$fTable][$fData['collectCol']];
+            $collectionColumn = $fData['collectCol'];
+            $filterColToRecount = $fData['filterCol'];//TODO, do this -> works only for ONE -> it is used for recounting ids
+            $filterTable        = $fTable;
+            $filterCondition = $dependData[$fTable][$collectionColumn];
         }
 
         $selCondition.= $conjunction.  " ".$fData['filterCol']." IN (".implode(',', $filterCondition).") ";
@@ -89,12 +95,46 @@ function doSelect ($table, $filterData, &$fixtures, $collect, $dependData=array(
         $defaultColumnsData[$table] = array();
     }
 
+    $recountVal = 1000;
+
     while($row = fktfetcharray($query, MYSQLI_ASSOC)) {
         $addFixtures .= '  -' ."\n";
 
+        // NOTE: keep order in this foreach,  must save recount column,then replace the id -> with new Recount value,  then use default
         foreach ($row as $colName => $colData) {
+
             if (in_array($colName, $collect)) {
                 $collectedData[$colName][] = $colData;
+            }
+
+            if (in_array($colName, $filterData['recountCol'])) {
+                if(array_key_exists($table, $recountData) && array_key_exists($colName, $recountData[$table])) {
+                    $recountVal = $recountData[$table][$colName]['countValue'];
+                }
+
+                // NOTE:  the colName in path does not allow for  table with  id, type, ref -> the ref start from 1001 for each type
+                $recountData[$table][$colName][$colData] = $recountVal++;
+                $recountData[$table][$colName]['countValue'] = $recountVal;
+
+                $colData = $recountData[$table][$colName][$colData];
+            } else if ($colName == $filterColToRecount) {
+
+                //[TODO -> bug v tabulce land   (nr neodpovida odkazu v zusatz_data  val
+                if ($table == 'land') {
+                    var_dump($recountData[$filterTable]);
+                    var_dump(sprintf("\033[41m.....\033[0m%s: ", "colData - colName").var_export($colName, 1));
+                    var_dump(sprintf("\033[41m.....\033[0m%s: ", "colData - collectionColumn").var_export($collectionColumn, 1));
+                }
+
+                //$colData = $recountData[$filterTable][$colName][$colData];
+                $colData = $recountData[$filterTable][$collectionColumn][$colData];
+            }
+
+            if (false && $table == 'land') {
+                var_dump(sprintf("\033[41m.....\033[0m%s: ", "filterColToRecount").var_export($filterColToRecount, 1));
+                var_dump(sprintf("\033[41m.....\033[0m%s: ", "recountData").var_export($recountData, 1));
+                var_dump(sprintf("\033[41m.....\033[0m%s: ", "filterTable").var_export($filterTable, 1));
+                var_dump(sprintf("\033[41m.....\033[0m%s: ", "collectionColumn").var_export($collectionColumn, 1));
             }
 
             if ((is_null($colData) || $colData == '') && in_array($colName, $defaultColumnsData[$table])) {
